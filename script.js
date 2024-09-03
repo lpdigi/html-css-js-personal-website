122e769dd314f12917b165d64ecd4a76b9e4c75b
lpdigi <lpdigi@umich.edu>
2024-09-03 12:02:10 -0400

    prepare for publishing

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("projectImg");
+var modalImg = document.getElementById("expandedImage");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
